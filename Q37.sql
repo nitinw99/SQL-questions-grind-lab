0ae17ba1fdf383531229d45b42149f4015ab9679
Nitin Wankvani <nitinwankvani@gmail.com>
2025-01-19 17:15:28 +0530

    question37-solved

---
+++ Q37.sql
@@ -0,0 +1,39 @@
+/*
+- Find the total number delayed orders for each delivery partner.
+
+CREATE TABLE swiggy_orders (
+    orderid INT PRIMARY KEY,
+    custid INT,
+    city VARCHAR(50),
+    del_partner VARCHAR(50),
+    order_time DATETIME,
+    deliver_time DATETIME,
+    predicted_time INT
+);
+INSERT INTO swiggy_orders (orderid, custid, city, del_partner, order_time, deliver_time, predicted_time)
+VALUES
+(1, 101, 'Mumbai', 'Partner A', '2024-12-18 10:00:00', '2024-12-18 11:30:00', 60),
+(2, 102, 'Delhi', 'Partner A', '2024-12-18 09:00:00', '2024-12-18 10:00:00', 45),
+(3, 103, 'Pune', 'Partner A', '2024-12-18 15:00:00', '2024-12-18 15:30:00', 30),
+(4, 104, 'Mumbai', 'Partner A', '2024-12-18 14:00:00', '2024-12-18 14:50:00', 45),
+(5, 105, 'Bangalore', 'Partner B', '2024-12-18 08:00:00', '2024-12-18 08:29:00', 30),
+(6, 106, 'Hyderabad', 'Partner B', '2024-12-18 13:00:00', '2024-12-18 14:00:00', 70),
+(7, 107, 'Kolkata', 'Partner B', '2024-12-18 10:00:00', '2024-12-18 10:40:00', 45),
+(9, 109, 'Chennai', 'Partner C', '2024-12-18 07:00:00', '2024-12-18 07:40:00', 30),
+(10, 110, 'Mumbai', 'Partner C', '2024-12-18 12:00:00', '2024-12-18 13:00:00', 50),
+(11, 111, 'Delhi', 'Partner C', '2024-12-18 09:00:00', '2024-12-18 09:35:00', 30),
+(12, 112, 'Hyderabad', 'Partner C', '2024-12-18 16:00:00', '2024-12-18 16:45:00', 30);
+
+
+*/
+
+-- SELECT * FROM swiggy_orders;
+
+WITH CTE AS (
+SELECT *, DATEDIFF(minute, ORDER_TIME, DELIVER_TIME) AS DELAY
+FROM swiggy_orders)
+
+SELECT DEL_PARTNER,
+    SUM(CASE WHEN DELAY>PREDICTED_TIME THEN 1 ELSE 0 END) AS TOTAL_DELAYED_ORDERS
+FROM CTE
+GROUP BY 1;
